821ce4c9aaa81bdc633fb295e8318bbbfe3bcd1b
GreyMatter's <greymattersyt@gmail.com>
2023-01-08 14:23:23 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -150,7 +150,7 @@ async def start(client, message):
                     file_id=msg.get("file_id"),
                     caption=f_caption,
                     protect_content=msg.get('protect', False),
-                    reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('@GreyMatter_Bots', url="https://t.me/GreyMatter_Bots") ] ] ),
+                    reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('@GreyMatter_Bots', url=https://t.me/GreyMatter_Bots) ] ] ),
                     
                 )
             except FloodWait as e:
@@ -161,7 +161,7 @@ async def start(client, message):
                     file_id=msg.get("file_id"),
                     caption=f_caption,
                     protect_content=msg.get('protect', False),
-                    reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('@GreyMatter_Bots', url="https://t.me/GreyMatter_Bots") ] ] ),
+                    reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('@GreyMatter_Bots', url=https://t.me/GreyMatter_Bots) ] ] ),
                 )
             except Exception as e:
                 logger.warning(e, exc_info=True)
@@ -223,7 +223,7 @@ async def start(client, message):
                 chat_id=message.from_user.id,
                 file_id=file_id,
                 protect_content=True if pre == 'filep' else False,
-                reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('@GreyMatter_Bots, url="https://t.me/GreyMatter_Bots") ] ] ),
+                reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('@GreyMatter_Bots, url=https://t.me/GreyMatter_Bots) ] ] ),
             )
             filetype = msg.media
             file = getattr(msg, filetype.value)
@@ -257,7 +257,7 @@ async def start(client, message):
         file_id=file_id,
         caption=f_caption,
         protect_content=True if pre == 'filep' else False,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('@GreyMatter_Bots', url="https://t.me/GreyMatter_Bots") ] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('@GreyMatter_Bots', url=https://t.me/GreyMatter_Bots) ] ] ),
     )
                     
 
